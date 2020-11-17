3ab86317e4f04090eb9e58f0601cd57c8a378ee3
ElioGaleotti87 <elio.galeotti00@gmail.com>
2020-11-17 16:38:39 -0300

    Comparation (Line 1 to 6) and Definitions (Line 8 to 37)

---
+++ exercises-examples.js
@@ -0,0 +1,39 @@
+Comparations 
+selector {
+	property = value;
+}
+
+var firstName = value;
+
+Definitios
+var variable: lugar, contenedor, ubicacion de almacenamiento donde guardar cosas para recuperarlas despues
+
+Las variables se pisan 
+var firstName = value01;
+var firstName = value02;
+firstName = value02
+firstName 
+value02
+
+Las variales se pueden rehusar para otras operaciones
+var realNumber = 13;
+realNumber + 12
+25
+
+const variable: la variable no puede ser pisada, variable constante
+const PI = 3.1416
+PI + realNumber
+16.1416
+
+palabras reservadas por el lenguaje var, const 
+
+firstName: se accede a lo que esta adentro de la variable por medio de ese nombre
+
+= operacion de asignacion (lo que esta a la derecha del = guardarlo en firstName)
+
+value: representacion de cosas
+
+Tipo de Dato: que tipo de cosas de guarda en var variable
+String: hilo de letras y o numeros
+
+
