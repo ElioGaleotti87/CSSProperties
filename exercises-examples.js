c9721ca8a811e36846bb368ec9d68cd3956bb14f
ElioGaleotti87 <elio.galeotti00@gmail.com>
2020-11-19 15:21:18 -0300

    Functions main concepts, coding group (Line 73 to 103)

--- exercises-examples.js
+++ exercises-examples.js
@@ -42,7 +42,70 @@ firstName: se accede a lo que esta adentro de la variable por medio de ese nombr
 value: representacion de cosas
 
 Tipo de Dato: que tipo de cosas de guarda en var variable
+
 String: hilo de letras y o numeros que se escriben entre comillas
+Numbers: var num, var real, var neg
+Boolean: 'to make decisions, you have to ask yourself a question' yes or no, true or false, 1 or 0
+true or false son palabras reservadas
+Null: nada, no es un 0, ni una palabra vacia
+undefined: no definido;
+
+Operadores Matematicos
++ - * / = % (Modulo) Modulo devuelve el resto de la division entera
+
+Operacion matematia con String
+'2' * '2'
+4
+'2' + '2'
+"22"
+ Casteo de datos: se cambia el tipo de dato
+ convertir tipo de datos para resolver lo que el usuario le estaba pidiendo
+ 
+ Logica interna
+'nombre ' + ' apellido'
+"nombre  apellido"
+ Suma de dos strings devuelve un string concatenado
+
+NaN (Not a Number)
+ null + undefined
+ NaN
+
+Funciones
+No escribir codigo que haga dos veces lo mismo
+verbos o acciones que hacen una persona o computadora
+Una funcion agrupa codigo, lo guarda en un lugar para luego ejecutarlo
+
+// console.log() recibe un argumento entre los parentesis y lo imprime
+
+console.log('Hola, mi nombre es sarasa,Guzman')
+console.log('estoy re pintado')
+
+function myFunction() {
+	//codigo
+}
+
+const otraFuncion = function() {
+	//codigo
+}
+
+//arrow function
+const yOtra = () => {
+	//codigo
+};
+
+function presentar(nombre,judicial) {
+	console.log('Hola mi nombre es ' + nombre)
+	console.log('Estoy re ' + judicial)
+};
+
+presentar('Mariano', 'imputado');
+Hola mi nombre es Mariano
+Estoy re imputado
+
+
+
+
+
 
 
 
