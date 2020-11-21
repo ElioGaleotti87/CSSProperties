bb4e8460eb5b93940cb19327162e7963c819b5f6
ElioGaleotti87 <elio.galeotti00@gmail.com>
2020-11-21 16:38:59 -0300

    Finish introduction to JS

--- exercises-examples.js
+++ exercises-examples.js
@@ -1,4 +1,4 @@
-Comparations 
+/Comparaciones/
 selector {
 	property = value;
 }
@@ -19,6 +19,22 @@ Las variales se pueden rehusar para otras operaciones
 var realNumber = 13;
 realNumber + 12
 25
+
+Argumentos
+son un estilo de variable que se declaran dentro de la funcion
+toman un valor luego cuando se las invocada
+function saluda(nombre){
+	console.log('Hola como va ' + nombre)
+};
+saluda('Mariano');
+Hola como va Mariano
+
+function suma(a, b){
+	console.log(a+b)
+};
+suma(1,10)
+11
+
 
 El lenguaje de programacion tiene una cantidad de operaciones que sabe hacer
 Presedencia de Signos (concepto matematico)
@@ -93,14 +109,203 @@ const yOtra = () => {
 	//codigo
 };
 
+
 function presentar(nombre,judicial) {
 	console.log('Hola mi nombre es ' + nombre)
 	console.log('Estoy re ' + judicial)
 };
 
-presentar('Mariano', 'imputado');
+presentar('Mariano', 'imputado'); /Invocar variable que ha sido declarada/
 Hola mi nombre es Mariano
 Estoy re imputado
+
+presentar(); /Invocar variable que no ha sido declarada/
+Hola mi nomre es undefined
+Estoy re undefined
+
+//..Crear - Invocar - Declarar o no variable..//
+
+Argumentos en Funciones
+Declarar un argumento en una funcion es como haber declarado una variable en otro Scope
+La variable no tiene valor aun, solo hasta que esta es invocada y se le asigna un valor
+
+
+
+
+Similitudes con Label y la relacion con Input en HTML
+
+
+
+
+
+Scope /Areas donde algunas variables tienen sentido y otras no/
+
+//Ejemplo de Scope//
+var nombre = Mariano;
+
+function reimputado (nombre){
+	console.log('Hola como andas imputado ' + nombre)
+};
+
+reimputado('Mati');
+Hola como andas imputado Mati
+
+me imprime Mati en vez de Mariano por una cuestion de Scope
+Cuando uno declara una funcion todo lo que este adentro de las llaves, se conoce como un Scope nuevo
+Las variables definidas dentro de una funcion no pueden ser accedidas desde afuera
+
+Como interactuan entre si los Scopes??
+
+var nombre = Mariano;
+
+function reimputado (){
+	console.log('Hola como andas imputado ' + nombre)
+};
+
+reimputado();
+Hola como andas imputado Mariano
+
+Cuando uno ejecuta en una function una variable que no fue declarada previamente como argumento
+javascript busca en el siguiente scope (area para encontrar sentido a la variable) e imprime ese valor al ser Invocada
+
+
+alert('asasa');
+
+prompt('sasas');
+Le tiras un string y te lo devuelve por medio de un input 
+
+Funciones matematicas
+Math. 
+
+
+
+Funciones Return
+
+function noRetorna(){
+	var hola = 1
+	var chao = 2
+};
+noRetorna();
+undefined
+
+var mariano = noRetorna(); //var mariano a lo que me devuelva noRtorna(), 
+//como las funciones sin argumentos no devuelven un valor especifico
+//me devuelve undefined cuando llamo a la variable mariano  
+mariano
+undefined
+
+function siRetorna(){
+	return 'La concha que te re pario'
+};
+siRetorna();
+"La concha que te remil pario"
+
+
+function siRetorna02(){
+	return 15
+};
+siRetorna02();
+15
+
+var mariano = SiRetorna02();
+mariano
+15
+
+SiRetorna02() + 12
+27
+
+suma (suma(3,4), suma(3,4))
+
+Todo el codigo que este abajo del return no va a funcionar, nunca se lo va a leer
+Las funciones pueden retornar una sola vez
+function suma(a, b){
+	console.log('hola')
+	return a + b
+	console.log('ciao') 
+};
+ 
+suma(1, 2);
+hola
+3
+
+
+ 
+
+Control de Flujo (condicionales)
+Permite agregar logica al codigo. 
+Ir haciendo preguntas e ir ejecutando distintos codigos segun ciertas condiciones
+
+function puedeManejar(edad){
+	if (edad > 18) { //Las condiciones son expresiones (rutas, caminos) que se convierten en verdadero o en falso (boolean)//
+		return true;
+	}
+		return false;
+}
+
+puedeManejar(22);
+true
+
+== se usa para comparar
+
+if(true){
+	console.log('hola')
+};
+hola
+
+if(false){
+	console.log('hola') //nunca va a salir hola porque la condificion
+};						//es falsa
+undefined
+
+
+Condiciones anidadas
+
+edad = 21
+21
+
+if (edad >=21){
+	console.log('puedes comprar alcohol');
+} else {
+	console.log('no puedes comprar alcohol');
+};
+puedes comprar alcohol
+
+edad=18
+18
+
+if (edad >=21){
+	console.log('puedes comprar alcohol');
+} else {
+	console.log('no puedes comprar alcohol');
+};
+no puedes comprar alcohol
+
+
+
+/Else If/
+
+edad = 19
+
+If(edad >=30){
+	console.log('sos viejo');
+}else if (edad >=20){
+	console.log('sos joven');
+} else {
+	console.log('estas re imputado');
+};
+estas re imputado
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
